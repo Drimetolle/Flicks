5debb73265fdf3864a742c50ea709a75a257e4ab
Drimetolle <drimetolle@gmail.com>
2022-07-23 14:09:56 +0500

    create discord client

--- core/src/discord_client.rs
+++ core/src/discord_client.rs
@@ -1,11 +1,18 @@
+use serenity::http::{Http, HttpBuilder};
+
 pub struct DiscordClient {
-    token: String,
+    client: Http,
 }
 
 impl DiscordClient {
     pub fn new(token: impl AsRef<str>) -> Self {
+        let mut client =  HttpBuilder::new("")
+            .ratelimiter_disabled(true)
+            .build();
+        client.token = token.as_ref().to_string();
+
         Self {
-            token: token.as_ref().to_string()
+            client
         }
     }
 }
