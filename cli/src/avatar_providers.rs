60cc72d2547f26b02a4aace8710bf42bf43d0706
Drimetolle <drimetolle@gmail.com>
2022-07-27 22:50:39 +0500

    implement base64 reader

--- cli/src/avatar_providers.rs
+++ cli/src/avatar_providers.rs
@@ -1,5 +1,8 @@
 use std::path::Path;
 use flicks_core::image::Image;
+use std::fs::File;
+use std::ffi::OsStr;
+use std::io::Read;
 
 pub trait AvatarProvider {
     fn get(&self, id: impl AsRef<str>) -> Option<Image>;
@@ -20,14 +23,27 @@ impl AvatarProvider for AvatarFileProvider {
         let name = id.as_ref().to_string();
         let path = Path::new(&self.base_path).join(&name);
 
-        // TODO implement read file as base64 
-        // let avatar = serenity::utils::read_image(path);
+        let avatar = self.read_image(path.as_ref());
 
-        // match avatar {
-        //     Ok(base64_image) => Some(Image::new(name, base64_image)),
-        //     Err(_) => None
-        // }
+        match avatar {
+            Ok(base64_image) => Some(Image::new(name, base64_image)),
+            Err(_) => None
+        };
 
         None
+    }
+}
+
+impl AvatarFileProvider {
+    fn read_image(&self, path: &Path) -> Result<String, std::io::Error> {
+        let mut v = Vec::default();
+        let mut f = File::open(path).unwrap();
+    
+        drop(f.read_to_end(&mut v));
+    
+        let b64 = base64::encode(&v);
+        let ext = if path.extension() == Some(OsStr::new("png")) { "png" } else { "jpg" };
+    
+        Ok(format!("data:image/{};base64,{}", ext, b64))
     }
 }
